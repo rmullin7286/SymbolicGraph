f129d2c19d5d8f2ac2b7f4df7f5169a11d0d9f43
rmullin7286 <ryan.mullin@wsu.edu>
2018-11-13 00:18:48 -0800

    Finished project?

---
+++ SymbolicGraph.py
@@ -0,0 +1,60 @@
+from pyeda.inter import *
+from functools import reduce
+
+
+def to_expr(li, varname):
+    bdd = bddvars(varname, 5)
+    varlist = [var if val else ~var for var, val in zip(bdd, li)]
+    return reduce(lambda a, b: a & b, varlist)
+
+
+def bool_array(num):
+    return [bool(num >> n & 1) for n in range(4, -1, -1)]
+
+
+def create_bool_formula(i, j):
+    x = to_expr(bool_array(i), 'x')
+    y = to_expr(bool_array(j), 'y')
+
+    return x & y
+
+
+def compose(r1, r2):
+    Z = bddvars('z', 5)
+    Y = bddvars('y', 5)
+    X = bddvars('x', 5)
+    for i in range(0, 4):
+        r1 = r1.compose({Y[i]: Z[i]})
+        r2 = r2.compose({X[i]: Z[i]})
+    return (r1 & r2).smoothing(Z)
+
+
+S = range(0, 31)
+G = [(i, j) for i in S for j in S if (i+3) % 32 == j % 32 or (i+7) % 32 == j % 32]
+exprs = [create_bool_formula(i, j) for i, j in G]
+F = reduce(lambda a, b: a | b, exprs)
+
+H = F
+while True:
+  Hp = H
+  H = Hp | compose(Hp, F)
+  if H.equivalent(Hp):
+      break
+
+S_bool = [bool_array(i) for i in S]
+
+P_total = True
+
+X = bddvars('x', 5)
+Y = bddvars('y', 5)
+
+P = H.smoothing(X).smoothing(Y).equivalent(True)
+
+print("P is true for all nodes i, j" if P else "P is false for all nodes i, j")
+
+
+
+
+
+
+
